6ad7bdb1784f2ec5eba3d3d858dd5e69f98d3083
Zane Baylon <zanebaylon@Zanes-MacBook-Pro.local>
2019-07-22 12:10:50 -0400

    add gitignore

---
+++ js/app.js
@@ -0,0 +1,34 @@
+// select the elements (define variables)
+const clear = document.querySelector(".clear"); // selects the clear button
+const dateElement = document.getElementById("date"); // will show today's date
+const list = document.getElementById("list"); // where our item lives
+const input = document.getElementById("input"); // gets what user puts in the field
+
+// classes names
+const CHECK = "fa-check-circle" // for the bottom check button
+const UNCHECK = "fa-circle-thin" // for the uncheck button
+const LINE_THROUGH = "lineThrough"; // for the line throught the text
+
+// show today's date
+const options = {weekday : "long", month : "short", day : "numeric"}; // defines the options for the date
+const today = new Date(); // defines the constant "today"
+
+dateElement.innerHTML = today.toLocaleDateString("en-US", options)
+
+// add to do function
+
+function addToDo(toDo){
+
+    // this is from the HTML file, defines each to do column
+    const item = `<li class="item"> 
+                <i class="fa fa-circle-thin co" job="complete" id="0"></i>
+                <p class="text">${toDo}</p>
+                <i class="fa fa-trash-o de" job="delete" id="0"></i>
+                </li>
+              `;
+    const position = "beforeend";
+
+    list.insertAdjacentHTML(position, item);
+}
+
+addToDo("drink coffee");
